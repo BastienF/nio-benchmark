9820653ae3b831f755e469a16ea191770ae1be71
BastienF <fiorentino.bastien@gmail.com>
2014-09-05 16:00:03 +0200

    correction run.sh

--- run.sh
+++ run.sh
@@ -60,12 +60,12 @@ java -jar $latencyServer &
 WAITPID_LATENCY=$!
 java -jar $cpuServer &
 WAITPID_CPU=$!
-sleep 20
+sleep 30
 mkdir results
-echo start warmup
-ab -r -k -c"$c" -n$(( $c + $n/10 )) $v "$cpuURL?cpu=$cpuTime&latency=$latencyTime" 1>/dev/null 2>/dev/null
-echo stop warmup
-sleep 5
+#echo start warmup
+#ab -r -k -c"$c" -n$(( $c + $n/10 )) $v "$cpuURL?cpu=$cpuTime&latency=$latencyTime" 1>/dev/null 2>/dev/null
+#echo stop warmup
+#sleep 5
 ab -r -k -c"$c" -n"$n" $v "$cpuURL?cpu=$cpuTime&latency=$latencyTime" 1>"results/users_$c""_async_$async""_cpu_$cpuTime""_lat_$latencyTime""_$date"".log"
 kill -9 $WAITPID_LATENCY
 kill -9 $WAITPID_CPU
